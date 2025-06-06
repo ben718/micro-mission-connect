68a5e602c930ea3f552f422556de7fb3682e2bf1
ben178 <benmvouama@gmail.com>
2025-06-06 10:47:25 +0200

    Suppression de tous les fichiers

--- src/types/user.ts
+++
@@ -1,8 +0,0 @@
-import { User as SupabaseUser } from "@supabase/supabase-js";
-
-export interface User extends SupabaseUser {
-  // Propriétés étendues pour la compatibilité avec l'authentification Supabase
-  // Ces propriétés sont utilisées pour déterminer le rôle de l'utilisateur
-  is_organization?: boolean;
-  role?: string;
-}
